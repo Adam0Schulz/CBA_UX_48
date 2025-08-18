2eed833dc3867683d8eb278c6ebee834d42dc090
Adam0Schulz <adam@schulz.sk>
2025-08-18 23:06:24 +0200

    added all components to the profile page and made current and past rentals pages

--- wireframe/src/App.js
+++ wireframe/src/App.js
@@ -1,22 +1,28 @@
-import './App.css';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Catalogue from './pages/Catalogue';
 import ToolDetail from './pages/ToolDetail';
 import Profile from './pages/Profile';
+import CurrentRentals from './pages/CurrentRentals';
+import PastRentals from './pages/PastRentals';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalogue" element={<Catalogue />} />
-        <Route path="/tool/:id" element={<ToolDetail />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalogue" element={<Catalogue />} />
+          <Route path="/tool/:id" element={<ToolDetail />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/current-rentals" element={<CurrentRentals />} />
+          <Route path="/past-rentals" element={<PastRentals />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
-
 
 export default App;
